c933d756e46153f01e91adf13d9348e13ca86e66
DavidPhongS <st026406@student.cichnovabrno.cz>
2025-10-13 15:16:58 +0200

    Monte Carlos Noglos Es que es qui es0

--- MyCodeMyLifeMyShit/kod.cpp
+++ MyCodeMyLifeMyShit/kod.cpp
@@ -2,21 +2,23 @@
 #include <stdlib.h>
 #include <math.h>
 #include <time.h>
+#define M_PI 3.14159265358979323846
+
 
 int main() {
-    int bod = 100000, bod_v_kruhu = 0;
-    float y
-    float x
+    float bod = 10000000, bod_v_kruhu = 0;
+    float y, x, distance;
 
-  for (int i = 0; i < bod; i++){
-    y = (float) rand() / RAND_MAX;
-    x = (float) rand() / RAND_MAX;
-    float distance = sqrt(x * x) + (y * y);
-if (distance <= 1){
-    bod_v_kruhu++;
-}
-  }
-float M_PI = 4 * bod_v_kruhu / bod;
-prinf("%f", M_PI);
-return 0;
+        for (int i = 0; i < bod; i++) {
+            y = (float)rand() / RAND_MAX;
+            x = (float)rand() / RAND_MAX;
+            distance = sqrt(x * x + y * y);
+            if (distance <= 1) {
+                bod_v_kruhu++;
+            }
+        }
+    float pie = 4 * (bod_v_kruhu / bod);
+    printf("%.10f", pie);
+    printf("\n%.10f", M_PI);
+    return 0;
 }
