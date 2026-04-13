1ddfb9d8c548f7613a8970c93cb5600c3946dae4
DavidPhongS <st026406@student.cichnovabrno.cz>
2026-04-13 11:36:26 +0200

    a

---
+++ MyCodeMyLifeMyShit/C ostry tip.cpp
@@ -0,0 +1,11 @@
+Console.Write (" " + i); //same as Console.Write ($(" {i}"));  '+' combine 2 strings tgt
+
+List<datovy tip> nazev = new List<datovy tip>(); //how to insert list
+
+nazev.Contains(neco) //obashuje neco
+
+nazev.Count //pocet prvku u listu
+
+numbers.Remove(2)//remove konkretne prvek
+
+numbers.RemoveAt(0)//remove index (misto)
